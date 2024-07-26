28e29f8347b41898db3677fdc7aa6998693afe29
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-07-26 07:57:40 +0530

    products in time 7:50

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,6 +2,7 @@ import "aos/dist/aos.css";
 import AOS from "aos";
 import "../Styles/Products.css";
 import { useState, useEffect } from "react";
+import Footer from "./Footer.jsx";
 
 function Products() {
   AOS.init({});
@@ -9,42 +10,52 @@ function Products() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch("http://localhost:3000/products")
       .then((response) => response.json())
       .then((data) => setData(data.data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
-    <div className="products-page">
-      <div className="products-header">
-        <h1> Our Products 🍓</h1>
-        <h3>Home / Products page</h3>
-      </div>
-      {data.map((product) => (
-        <div key={product._id} data-aos="zoom-in-up" className="animation">
-          <div className="products-image">
-            <img width={"300px"} height={"200px"} src={product.image_url} alt="product-image" />
-          </div>
-          <div className="about-products">
-            <div className="product-name">
-              <h5>{product.name}</h5>
-              <p>
-                {" "}
-                <b>Price: {product.price}</b>
-              </p>
-            </div>
-            <div className="product-discriptions">
-              <p>{product.description}</p>
-            </div>
-            <div className="product-btn">
-              <button>BUY</button>
-              <button>Add to Cart</button>
-            </div>
-          </div>
+    <>
+      <div className="products-page">
+        <div className="products-header">
+          <h1> Our Products 🍓</h1>
+          <h3>Home / Products page</h3>
         </div>
-      ))}
-    </div>
+        <div className="products-grid">
+          {data.map((product) => (
+            <div key={product._id} data-aos="zoom-in-up" className="animation">
+              <div className="products-image">
+                <img
+                  width={"300px"}
+                  height={"200px"}
+                  src={product.image_url}
+                  alt="product-image"
+                />
+              </div>
+              <div className="about-products">
+                <div className="product-name">
+                  <h5>{product.name}</h5>
+                  <p>
+                    {" "}
+                    <b>Price: {product.price}</b>
+                  </p>
+                </div>
+                <div className="product-discriptions">
+                  <p>{product.description}</p>
+                </div>
+                <div className="product-btn">
+                  <button>BUY</button>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
